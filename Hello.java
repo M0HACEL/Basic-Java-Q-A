4b0a39e99e5cef391632ed7969d95c9ae92ed70d
Md. Mohacel Hosen <96243118+M0HACEL@users.noreply.github.com>
2022-10-03 19:50:30 +0600

    print 'Hello, your name and a emoji' on the screen
    1. Write a Java program to print 'Hello, your name and a smile emoji' on screen and then print  "Welcome to the world of JAVA" on a separate line.
    Hint: Character.toString( 129300 )
    ref: https://www.quackit.com/character_sets/emoji/emoji_v3.0/unicode_emoji_v3.0_characters_all.cfm

---
+++ Hello.java
@@ -0,0 +1,7 @@
+package Basic_Java;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello, Mohacel Hosen"+Character.toString( 128_512 )+".\nWelcome to the world of JAVA");
+    }
+}
